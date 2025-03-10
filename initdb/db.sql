866a8439929a50e4349a22abcf4ab1dc143d0372
nahuelDAM <wgf90900@educastur.es>
2025-03-10 17:18:07 +0100

    Cambios 3

--- initdb/db.sql
+++ initdb/db.sql
@@ -18,8 +18,8 @@
 --
 -- Table structure for table `campeonatos`
 --
-CREATE DATABASE panaurum;
-USE panaurum;
+CREATE DATABASE panaurum_db;
+USE panaurum_db;
 DROP TABLE IF EXISTS `campeonatos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
